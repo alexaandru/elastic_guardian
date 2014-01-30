663893d107b4fbd9193c3e70aa4c92b5d8651d2b
Alex Ungur <alexaandru@gmail.com>
2014-01-30 10:44:41 +0200

    Updated unit tests

--- authentication/authentication_test.go
+++ authentication/authentication_test.go
@@ -9,31 +9,31 @@ var foobar, foobogus = base64.StdEncoding.EncodeToString([]byte("foo:bar")),
 	base64.StdEncoding.EncodeToString([]byte("foo:bogus"))
 
 func TestShouldFailIfAuthHeaderEmpty(t *testing.T) {
-	if ok, _ := BasicAuthPassed(""); ok {
+	if status, _ := BasicAuthPassed(""); status != NotAttempted {
 		t.Error("BasicAuthPassed() should be false with empty header")
 	}
 }
 
 func TestShouldFailIfNotBasicAuth(t *testing.T) {
-	if ok, _ := BasicAuthPassed("Basicx foo"); ok {
+	if status, _ := BasicAuthPassed("Basicx foo"); status != NotBasic {
 		t.Error("BasicAuthPassed() should be false with non Basic header")
 	}
 }
 
 func TestShouldFailIfNonDecodable(t *testing.T) {
-	if ok, _ := BasicAuthPassed("Basic garbageHere"); ok {
+	if status, _ := BasicAuthPassed("Basic garbageHere"); status != Failed {
 		t.Error("BasicAuthPassed() should be false with non Base64 data")
 	}
 }
 
 func TestShouldFailWithIncorrectPassword(t *testing.T) {
-	if ok, _ := BasicAuthPassed("Basic " + foobogus); ok {
+	if status, _ := BasicAuthPassed("Basic " + foobogus); status != Failed {
 		t.Error("BasicAuthPassed() should be false with incorrect password")
 	}
 }
 
 func TestShouldPass(t *testing.T) {
-	if ok, _ := BasicAuthPassed("Basic " + foobar); !ok {
+	if status, _ := BasicAuthPassed("Basic " + foobar); status != Passed {
 		t.Error("BasicAuthPassed() should be true with correct password")
 	}
 }
